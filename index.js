6ce7bd54f399c8215a74db583fbd534d67064aaf
hassan302-stack <kazmihassan336@gmail.com>
2021-08-25 16:58:51 +0500

    second

--- index.js
+++ index.js
@@ -3,53 +3,68 @@ let select= document.getElementById("select")
 let NewCases =document.querySelector(".NewCases")
 let TotalCases =document.querySelector(".TotalCases")
 let Deaths =document.querySelector(".Deaths")
-fetch("http://restcountries.eu/rest/v2/all")
-.then((res)=>{
-    let response= res.json();
-    console.log(response)
-    return response
+let wNewCases =document.querySelector(".wNewCases")
+let wTotalCases =document.querySelector(".wTotalCases")
+let wDeaths =document.querySelector(".wDeaths")
+// fetch("http://restcountries.eu/rest/v2/all")
+// .then((res)=>{
+//     let response= res.json();
+//     console.log(response)
+//     return response
     
-})
-.then((info)=>{
-    let index=0;
-  html=""
-  info.forEach(element => {
+// })
+// .then((info)=>{
+//     let index=0;
+//   html=""
+//   info.forEach(element => {
     
-      html =html+`<option value="${index}">${element.name}</option>`
-      index++
-  });
-  console.log(html);
-  select.insertAdjacentHTML("beforeend",html)
-})
+//       html =html+`<option value="${index}">${element.name}</option>`
+//       index++
+//   });
+//   console.log(html);
+//   select.insertAdjacentHTML("beforeend",html)
+// })
 
 select.addEventListener('change',()=>{
     debugger
    let val= Number(select.value)
 console.log(val);
+callApi(val)
 
-fetch("https://api.covid19api.com/summary")
-.then((res)=>{
-    let response= res.json();
-    console.log(response)
-    return response
+})
+  
+
+
+function callApi(val=0){
+    fetch("https://api.covid19api.com/summary")
+    .then((res)=>{
+        let response= res.json();
+        console.log(response)
+        return response
+        
+    })
+    .then((data)=>{
+       html=""
+        let cont=data.Countries;
+        cont.forEach((element,index)=>{
+            html =html+`<option value="${index}">${element.Country}</option>`
+            
+        })
+        select.insertAdjacentHTML("beforeend",html)
+        console.log(cont);
+        NewCases.innerHTML = cont[val].NewConfirmed
+        TotalCases.innerHTML = cont[val].TotalConfirmed
+        Deaths.innerHTML = cont[val].TotalDeaths
+        wNewCases.innerHTML = data.Global.NewConfirmed
+        wTotalCases.innerHTML = data.Global.TotalConfirmed
+        wDeaths.innerHTML = data.Global.TotalDeaths
+
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
     
-})
-.then((data)=>{
-   
-    let cont=data.Countries;
-   console.log(cont[val].Country);
-   console.log(cont[val].NewConfirmed);
-   console.log(cont[val].TotalConfirmed);
-   console.log(cont[val].TotalDeaths);
-    console.log(cont);
-    NewCases.innerHTML = cont[val].NewConfirmed
-    TotalCases.innerHTML = cont[val].TotalConfirmed
-    Deaths.innerHTML = cont[val].TotalDeaths
-})
-.catch((error)=>{
-    console.log(error);
-})
+    
 
-
-})
-
+}
+callApi()
